aaca4ba3ca5ddacefea89966bd043d8ee77c7b53
123vivekr <123vivekr@gmail.com>
2021-12-03 11:22:57 +0530

    ADD. RWMutexLock to make APIs thread safe

--- in_memory_score_store.go
+++ in_memory_score_store.go
@@ -1,17 +1,24 @@
 package main
 
+import "sync"
+
 func NewInMemoryScoreStore() *InMemoryScoreStore {
-	return &InMemoryScoreStore{map[string]int{}}
+	return &InMemoryScoreStore{sync.RWMutex{}, map[string]int{}}
 }
 
 type InMemoryScoreStore struct {
+	lock  sync.RWMutex
 	store map[string]int
 }
 
 func (i *InMemoryScoreStore) GetPlayerScore(name string) int {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
 	return i.store[name]
 }
 
 func (i *InMemoryScoreStore) RecordWin(name string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	i.store[name]++
 }
